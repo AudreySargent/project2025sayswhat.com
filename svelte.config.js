54e0b36850c5b83028298e05f92d073208f34efd
Michael V. Colianna <michael.colianna@me.com>
2024-07-18 16:47:11 -0400

    Removes config log and adds check for development

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapterNetlify from '@sveltejs/adapter-netlify'
 import adapterStatic from '@sveltejs/adapter-static'
 
-console.log('config', process.env.NODE_ENV)
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: process.env.NODE_ENV
+		adapter: process.env.NODE_ENV === 'development'
 			? (() => {
 				console.log(`\x1b[2m\x1b[32m*** Using static adapter ***\x1b[0m`)
 
