ff4167a564ba9beb845c3c67df2cc4264afcd36c
Michael V. Colianna <michael.colianna@me.com>
2024-07-18 16:45:17 -0400

    Updates config for adapter since Netlify thinks it was using auto

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: process.env.NODE_ENV ? adapterStatic() : adapterNetlify(),
+		adapter: process.env.NODE_ENV
+			? (() => {
+				console.log(`\x1b[2m\x1b[32m*** Using static adapter ***\x1b[0m`)
+
+				return adapterStatic({})
+			})()
+			: (() => {
+				console.log(`\x1b[2m\x1b[32m*** Using Netlify adapter ***\x1b[0m`)
+
+				return adapterNetlify({})
+			})(),
 	},
 	preprocess: vitePreprocess(),
 }
