432071bdc6762a763cf4c5cd692f04ff8d79505f
Michael V. Colianna <michael.colianna@me.com>
2024-07-20 00:14:44 -0400

    (wip) Slightly adjusts cards even though they will eventually change

--- src/components/card.js
+++ src/components/card.js
@@ -11,25 +11,29 @@ const Card = ({
 }) => {
   return (
     <details className="group p-4">
-      <summary className="flex gap-4 justify-between items-center font-medium cursor-pointer list-none">
-        <span className="font-serif font-bold text-xl">{headline}</span>
-        <span className="group-open:rotate-180">
-          <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
-        </span>
+      <summary className="font-medium cursor-pointer list-none">
+        <div className="mx-auto max-w-screen-xl w-full flex gap-4 justify-between items-center">
+          <span className="font-serif font-bold text-xl">{headline}</span>
+          <span className="group-open:rotate-180">
+            <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+          </span>
+        </div>
       </summary>
 
-      <div className="mt-9 grid gap-8 lg:grid-cols-2 lg:items-start">
-        <RichText content={details} />
+      <div className="mx-auto max-w-screen-xl w-full">
+        <div className="mt-9 grid gap-8 lg:grid-cols-2 lg:items-start">
+          <RichText content={details} />
 
-        <div className="grid gap-4">
-          <div className="font-bold">Further Explanation</div>
+          <div className="grid gap-4">
+            <div className="font-bold">Further Explanation</div>
 
-          <RichText content={explanation} />
+            <RichText content={explanation} />
+          </div>
         </div>
-      </div>
 
-      <div className="flex gap-2 items-center mt-4 text-sm">
-        <ExternalLink href={sourceUrl}>Source: {source}</ExternalLink>
+        <div className="flex gap-2 items-center mt-4 text-sm">
+          <ExternalLink href={sourceUrl}>Source: {source}</ExternalLink>
+        </div>
       </div>
     </details>
   )
